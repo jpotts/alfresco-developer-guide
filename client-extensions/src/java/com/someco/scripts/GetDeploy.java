1994237370f559bfe4c6aa6a340f7fcd0ed3b2b2
jeffpotts01@gmail.com <jeffpotts01@gmail.com@ecb500a3-8c0a-b885-a80f-70c392eb39ab>
2011-01-17 19:37:03 +0000

    initial population

--- client-extensions/src/java/com/someco/scripts/GetDeploy.java
+++ client-extensions/src/java/com/someco/scripts/GetDeploy.java
@@ -30,10 +30,9 @@ import org.alfresco.service.namespace.QName;
 import org.alfresco.util.GUID;
 import org.alfresco.util.ISO9075;
 import org.alfresco.web.bean.wcm.AVMUtil;
-import org.alfresco.web.scripts.Cache;
 import org.alfresco.web.scripts.DeclarativeWebScript;
-import org.alfresco.web.scripts.Status;
 import org.alfresco.web.scripts.WebScriptRequest;
+import org.alfresco.web.scripts.WebScriptStatus;
 import org.apache.log4j.Logger;
 
 public class GetDeploy extends DeclarativeWebScript {
@@ -52,8 +51,7 @@ public class GetDeploy extends DeclarativeWebScript {
 	private Logger logger = Logger.getLogger(GetDeploy.class);
 	
 	@Override
-	protected Map<String, Object> executeImpl(WebScriptRequest req,
-			Status status, Cache cache) {
+	protected Map<String, Object> executeImpl(WebScriptRequest req, WebScriptStatus status) {
 		// declare the model object we're going to return
 		Map<String, Object> model = new HashMap<String, Object>();
 
@@ -63,7 +61,9 @@ public class GetDeploy extends DeclarativeWebScript {
 		model.put("store", store);
 		
 		if (store == null || store.length() == 0) {
-			status.setCode(Status.STATUS_BAD_REQUEST, "Store is a required parameter.");
+			status.setCode(400);			
+			status.setMessage("Store is a required parameter.");
+			status.setRedirect(true);
 			return model;
 		}
 
@@ -76,7 +76,9 @@ public class GetDeploy extends DeclarativeWebScript {
 	                                     SandboxConstants.PROP_WEB_PROJECT_NODE_REF).getValue(DataTypeDefinition.NODE_REF);
        
 		if (webProjectNodeRef == null || !nodeService.exists(webProjectNodeRef)) {
-			status.setCode(Status.STATUS_BAD_REQUEST, "Web project does not exist.");
+			status.setCode(500);			
+			status.setMessage("Web project does not exist.");
+			status.setRedirect(true);
 			return model;			
 		}
 		
@@ -87,7 +89,9 @@ public class GetDeploy extends DeclarativeWebScript {
 		if (allocServerList.isEmpty()) {
 			List<NodeRef> serverList = findDeployToServers(webProjectNodeRef, null);
 			if (serverList.isEmpty()) {
-				status.setCode(Status.STATUS_BAD_REQUEST, "No test servers are available.");
+				status.setCode(500);			
+				status.setMessage("No test servers are available.");
+				status.setRedirect(true);
 				return model;
 	        }
 			serverRef = serverList.get(0);
---
+++ server-extensions/readme.txt
@@ -0,0 +1,29 @@
+The someco-server-extensions project can be imported as an existing Eclipse project into the Eclipse IDE. If the Alfresco SDK has been imported into the same Eclipse workspace, the project will have everything it needs to build successfully.
+
+Building the project with Ant
+
+The project includes an build.xml and a build.properties. If you do not modify the project folder structure, you should only have to modify the build.properties file to specify the location of the Alfresco SDK, Alfresco web application root, and Alfresco "shared" folders.
+
+In prior versions of this source code, enterprise and community configs were in the same project. When you did the deploy, you specified which flavor you wanted. That's no longer the case now that the source code has been split by chapter, by version.
+
+WARNING!
+
+This project contains the files you would typically put in your Tomcat shared extension folder. That's things like your custom-repository.properties file. If you have been maintaining your own set of props files and have not imported those files into this project, DO NOT DEPLOY THIS PROJECT. It will overwrite your custom settings that are sitting in Alfresco shared extensions.
+
+To deploy:
+
+1. Make sure build.properties reflects the appropriate file paths.
+2. Run "ant deploy".
+
+Known working LDAP setups
+
+This config (with minor tweaks for schema differences) has been known to work with both OpenLDAP and Apache Directory Server. If you use the LDIF files in the data directory to build your initial context in your LDAP directory, note the directory-specific root.ldif files.
+
+Known working CAS setups
+
+The CAS-related config in this project has been known to work with:
+
+JA-SIG CAS Server 3.2.1 + Yale CAS Client 2.1.1
+JA-SIG CAS Server 3.3.3 + Yale CAS Client 2.1.1
+
+It will not work as-is with the JA-SIG CAS Client for Java.
