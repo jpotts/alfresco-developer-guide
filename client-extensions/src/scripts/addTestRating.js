1ab82f27b1d7ccae1a90f84f31552481a8536f9b
jeffpotts01@gmail.com <jeffpotts01@gmail.com@ecb500a3-8c0a-b885-a80f-70c392eb39ab>
2011-01-31 04:23:42 +0000

    Chapters 2 - 9 3.4 Enterprise

--- client-extensions/src/scripts/addTestRating.js
+++ client-extensions/src/scripts/addTestRating.js
@@ -1,8 +1,3 @@
-// randomly pick a num b/w 1 and 5 inclusive
-var ratingValue = Math.floor(Math.random()*5) + 1;
-
-// use the rating service instead
-/*
 // add the aspect to this document if it needs it
 if (document.hasAspect("sc:rateable")) {
   logger.log("Document already as aspect");
@@ -11,6 +6,9 @@ if (document.hasAspect("sc:rateable")) {
   document.addAspect("sc:rateable");
 }
 
+// randomly pick a num b/w 1 and 5 inclusive
+var ratingValue = Math.floor(Math.random()*5) + 1;
+
 var props = new Array(2);
 props["sc:rating"] = ratingValue;
 props["sc:rater"] = person.properties.userName;
@@ -18,7 +16,4 @@ props["sc:rater"] = person.properties.userName;
 // create a new ratings node and set its properties
 var ratingsNode = document.createNode("rating" + new Date().getTime(), "sc:rating", props, "sc:ratings");
 ratingsNode.save();
-*/
-
-ratings.rate(document, ratingValue, person.properties.userName);
 logger.log("Ratings node saved.");
